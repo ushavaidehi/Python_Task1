3053e55ed6e0a1983e5b03ca7c0ab21fc16ae15b
Usha <42339544+ushavaidehi@users.noreply.github.com>
2020-06-27 21:25:59 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+print("          THE POOL PLAYERS.    \n          SEVEN AT THE GOLDEN SHOVEL. \nWe real cool. We\nLeft school. We\n\nLurk late. We\nStrike straight. We\n\nSing sin. We\nThin gin. We")
+
+
+
+
