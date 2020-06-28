67b353fbdedad5e3ce3c8fa62c66763a359f8ea0
Usha <42339544+ushavaidehi@users.noreply.github.com>
2020-06-28 09:27:27 +0530

    Details of Date & Time

---
+++ currentdate.py
@@ -0,0 +1,24 @@
+import datetime
+
+current_date = datetime.datetime.now()
+
+print("The attributes of now() are : ")
+
+print("Year: ", end= "")
+print(current_date.year)
+
+print("month: ", end= "")
+print(current_date.month)
+
+print("day: ", end= "")
+print(current_date.day)
+
+print("hour: ",end= "")
+print(current_date.hour)
+
+print("minute: ", end= "")
+print(current_date.minute)
+
+
+
+
